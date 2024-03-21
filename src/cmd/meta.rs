540727b94527f9be8f3f88b835889c8f7be77e66
Jasper Li <liyang.jasper@gmail.com>
2024-03-21 18:31:17 +0800

    select column and row for meta

--- src/cmd/meta.rs
+++ src/cmd/meta.rs
@@ -8,16 +8,19 @@ use crate::cmd::utils::*;
 #[derive(Parser, Debug)]
 pub struct Args {
     #[arg(short, long)]
-    group: Option<usize>,
+    group: Vec<i32>,
 
-    files: String,
+    file: String,
 
     #[arg(short, long, default_value_t = false)]
-    statics: bool,
+    schema: bool,
+
+    #[arg(short, long)]
+    column: Vec<i32>,
 }
 
 pub fn meta_main(args: Args) -> eyre::Result<()> {
-    let file = args.files;
+    let file = args.file;
     let file = open_file(file)?;
     let parquet_reader = SerializedFileReader::new(file)?;
 
@@ -28,38 +31,62 @@ pub fn meta_main(args: Args) -> eyre::Result<()> {
     println!("total columns: {}", file_meta.schema_descr().num_columns());
     println!("total groups: {}", c);
 
-    let columns = file_meta.schema_descr().columns();
-    println!("column types:");
-    for i in 0..columns.len() {
-        println!(
-            "\t{i} {}: {} {:?}",
-            columns[i].name(),
-            columns[i].converted_type(),
-            columns[i].logical_type()
-        );
+    if args.schema {
+        let columns = file_meta.schema_descr().columns();
+        println!("column types:");
+        for i in 0..columns.len() {
+            println!(
+                "\t{i} {}: {} {:?}",
+                columns[i].name(),
+                columns[i].converted_type(),
+                columns[i].logical_type()
+            );
+        }
     }
 
-    println!("row group summaries");
+    let col_sets = hashset(args.column.clone());
+    let row_sets = hashset(args.group.clone());
+
+    println!("row group information:");
     for i in 0..c {
         let reader = parquet_reader.get_row_group(i).unwrap();
         let rg_metadata = reader.metadata();
-        println!(
-            "\tRow group {i} has {} rows, {} bytes , {} columns, sorting columns is {:?}",
-            rg_metadata.num_rows(),
-            rg_metadata.total_byte_size() + rg_metadata.compressed_size(),
-            rg_metadata.num_columns(),
-            rg_metadata.sorting_columns()
-        );
-        // for i in 0..rg_metadata.columns().len() {
-        //     println!(
-        //         "\t\tcolumn {i}: {} => {} by {}, encoding: {:?}, statics: {:?}",
-        //         rg_metadata.column(i).uncompressed_size(),
-        //         rg_metadata.column(i).compressed_size(),
-        //         rg_metadata.column(i).compression(),
-        //         rg_metadata.column(i).encodings(),
-        //         rg_metadata.column(i).statistics()
-        //     );
-        // }
+        match args.group.len() + args.column.len(){
+            0 => {
+                println!(
+                    "\tRow group {i} has {} rows, {} bytes , {} columns, sorting columns is {:?}",
+                    rg_metadata.num_rows(),
+                    rg_metadata.compressed_size(),
+                    rg_metadata.num_columns(),
+                    rg_metadata.sorting_columns()
+                );
+            }
+            _ => {
+                if row_sets.is_empty() || row_sets.contains(&(i as i32)) {
+                    //println!("\tDump Row group {i}");
+                    println!(
+                        "\tRow group {i} has {} rows, {} bytes , {} columns, sorting columns is {:?}",
+                        rg_metadata.num_rows(),
+                        rg_metadata.compressed_size(),
+                        rg_metadata.num_columns(),
+                        rg_metadata.sorting_columns()
+                    );
+                    for j in 0..rg_metadata.columns().len() {
+                        if col_sets.is_empty() || col_sets.contains(&(j as i32)) {
+                            println!(
+                                "\t\tcolumn {j}: {} => {} by {}, encoding: {:?}, statics: {:?}",
+                                rg_metadata.column(j).uncompressed_size(),
+                                rg_metadata.column(j).compressed_size(),
+                                rg_metadata.column(j).compression(),
+                                rg_metadata.column(j).encodings(),
+                                rg_metadata.column(j).statistics()
+                            );
+                        }
+
+                    }
+                }
+            }
+        }
     }
 
     Ok(())
